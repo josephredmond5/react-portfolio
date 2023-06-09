c8a7eddfd13ca0a6c4b1749042e05549cdb8669d
josephredmond5 <josephredmond5@icloud.com>
2023-06-09 15:00:42 +0100

    added projectDisplay and projectdisplay.css code

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,27 @@
+import Estate from "../assets/estate.jpg";
+import Kits from "../assets/kits.jpg";
+import Portfolio from "../assets/portfolio.jpg";
+import Weather from "../assets/weather.jpg";
+
+export const ProjectList = [
+  {
+    name: "Surreal Estate",
+    image: Estate,
+    skills: "Javascript, HTML, CSS",
+  },
+  {
+    name: "kitstack",
+    image: Kits,
+    skills: "React, Firebase, Javascript, HTML, CSS",
+  },
+  {
+    name: "portfolio",
+    image: Portfolio,
+    skills: "React, Javascript, HTML, CSS"
+  },
+  {
+    name: "weather app",
+    image: Weather,
+    skills: "Javascript, HTML, CSS",
+  },
+];

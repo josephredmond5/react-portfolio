50d977044ba8448d01d4422f270dfc08febcd7a8
josephredmond5 <josephredmond5@icloud.com>
2023-06-09 02:36:14 +0100

    navbar.css code added

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import {Link} from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from '@material-ui/icons/Reorder';
+
+function Navbar() {
+        const [expandNavbar, setExpandNavbar] = useState(false);
+    
+    return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+    <div className="toggleButton">
+    <button onClick={() => {
+        setExpandNavbar((prev) => !prev);
+    }}
+    >
+        <ReorderIcon /> 
+        </button>
+        </div>
+        <div className='links'> 
+            <Link to="/"> Home</Link>
+            <Link to="/projects"> projects</Link>
+            <Link to="/experience"> Experience</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
